3422b903b5f845cd61243a5ac87350acf6a4566b
Will Usher <william.usher@ouce.ox.ac.uk>
2019-06-28 11:51:16 +0200

    Adds an example of how to send out mails using Python

---
+++ mailing/README.md
@@ -0,0 +1,4 @@
+# Send e-mails using Python
+
+You can easily send e-mails using Python which include attachments. This allows
+you to attach custom files (such as letters) which are unique for each participant
---
+++ mailing/mailing.py
@@ -0,0 +1,67 @@
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+def send_mail(send_from, send_to, subject, text, files=None,
+              server="127.0.0.1"):
+    """Formulates an e-mail and sends it
+    """
+    assert isinstance(send_to, list)
+
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text))
+
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(
+                fil.read(),
+                Name=basename(f)
+            )
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        msg.attach(part)
+
+    smtp = smtplib.SMTP(server)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.close()
+
+
+def get_text(name):
+    text = """
+Dear {},
+
+I am writing to you on behalf of Prof. Boffin and the MASSIVE Consortium regarding our response to the Horizon 2020 research call.
+
+We look forward to your response.
+
+Kind regards,
+
+Mr. Brainiac
+
+
+""".format(name)
+    return text
+
+with open("../Stakeholder database_E3M.csv") as csvfile:
+    reader = DictReader(csvfile, fieldnames=['name', 'email'])
+
+    next(reader)
+    for index, row in enumerate(reader):
+    
+        email = row['email']
+        print("This email will be sent to {}".format(email))
+        send_mail('wusher@kth.se',
+                  [email],
+                  "Request for a letter of support",
+                  get_text(row['name']),
+                  files=['./split/CLEAR_Stakeholder_Letter_{}.pdf'.format(filename),
+                         '../CLEAR_Stakeholders_letter_template.docx'],
+                  server='smtp.kth.se')
