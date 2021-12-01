3c7c1f1437338c40a76242afe5b9adbf8f2df4c6
HauHe <haukeh@kth.se>
2021-12-01 11:43:12 +0100

    run.py now able to read, run and solv lp files and to produce a csv with dual values. Adaped snakefile to call run.py for running lp files.

--- osemosys_workflow/run.py
+++ osemosys_workflow/run.py
@@ -1,22 +1,35 @@
 """This script runs OSeMOSYS models using gurobi. It takes as input an lp-file and produces a sol-file.
 """
 import sys
+import os
 import gurobipy
+import pandas as pd
 
-def sol_gurobi(lp_path: str, ilp_path: str, outpath: str):
+def sol_gurobi(lp_path: str):
     #lp_path = "data/utopia2/utopia2.lp" #for testing
     m = gurobipy.read(lp_path)
     m.optimize()
-    dual = m.Pi
-    constr = m.getConstrs()
-    with open('results/dual.csv', 'w') as f:
-        for i in range(len(dual)):
-            f.write("%(const)s,%(dual)s\n" % {'const': constr[i], 'dual': dual[i]})
+
+    return m
+
+def get_dual(model):
+    dual = model.Pi
+    constr = model.getConstrs()
+    df_dual = pd.DataFrame(dual, index=constr, columns=['value'])
+    return df_dual
+
+def write_dual(df_dual: pd.DataFrame, path: str):
+    path_res = os.sep.join(path.split('/')[:-1]+['results_csv'])
+    os.mkdir(path_res)
+    df_dual.to_csv('%(path)s/Dual.csv' % {'path': path_res})
+    return
+
+def write_sol(sol, path_out: str, path_gen: str):
     try:
-        m.write(outpath)
+        sol.write(path_out)
     except:
-        m.computeIIS()
-        m.write(ilp_path)
+        sol.computeIIS()
+        sol.write("%(path)s.ilp" % {'path': path_gen})
     return
 
 if __name__ == "__main__":
@@ -24,11 +37,14 @@ if __name__ == "__main__":
     args = sys.argv[1:]
 
     if len(args) != 3:
-        print("Usage: python run.py <lp_path> <ilp.path> <output_path>")
+        print("Usage: python run.py <lp_path> <generic_path> <output_path>")
         exit(1)
 
     lp_path = args[0]
-    ilp_path = args[1]
+    gen_path = args[1]
     outpath = args[2]
     
-    sol_gurobi(lp_path, ilp_path, outpath)
+    model = sol_gurobi(lp_path)
+    df_dual = get_dual(model)
+    write_dual(df_dual, gen_path)
+    write_sol(model, outpath, gen_path)
